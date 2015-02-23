64c924b1e7215b4f2a69f6f783d6281ca2ae1771
juliocesar <juliocesarrodriguezcruz@gmail.com>
2015-02-23 19:10:59 +0000

    Added Window basic model.

--- windows/tests.py
+++ windows/tests.py
@@ -3,6 +3,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 from django.test import TestCase
 from django.views.generic import CreateView
+from windows.models import Window
 from windows.views import WindowDetailView
 
 
@@ -20,6 +21,23 @@ class HomePageTest(TestCase):
         self.assertEqual(response.status_code, 200)
         expected_html = render_to_string('windows/window_detail.html')
         self.assertEqual(response.rendered_content.decode(), expected_html)
+
+
+class WindowModelTest(TestCase):
+
+    # TODO: Inherit from ModelTestHelper
+
+    def test_saving_and_retrieving_windows(self):
+        Window.objects.create(title="First Window Title", slug="window1")
+        Window.objects.create(title="Second Window Title", slug="window2")
+
+        saved_windows = Window.objects.all()
+        self.assertEqual(saved_windows.count(), 2)
+
+        first_saved_window = saved_windows[0]
+        second_saved_window = saved_windows[1]
+        self.assertEqual(first_saved_window.title, "First Window Title")
+        self.assertEqual(second_saved_window.title, "Second Window Title")
 
 
 class WindowAddTest(TestCase):
