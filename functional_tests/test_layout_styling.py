df8d592e29ead88d965f1651005951bfccf42b22
juliocesar <juliocesarrodriguezcruz@gmail.com>
2015-03-15 11:15:46 -0400

    Fixed layout test and base template.

--- functional_tests/test_layout_styling.py
+++ functional_tests/test_layout_styling.py
@@ -7,5 +7,5 @@ class LayoutAndStylingTest(FunctionalTest):
         # Visitor goes to the home page
         self.browser.get(self.server_url)
         # He notice user area is at top right
-        user_area = self.browser.find_element_by_id('welcome')
+        user_area = self.browser.find_element_by_id('navbar-tray')
         self.assertAlmostEqual(user_area.location['y'], 0, delta=20)
