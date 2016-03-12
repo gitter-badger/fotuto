e88a41308c18312f0061e3b4f7f542ea1d741a6d
Julio Cesar <juliocesarrodriguezcruz@gmail.com>
2016-03-11 21:46:58 -0500

    fotuto #1: Add test of api token authorization

--- functional_tests/test_api.py
+++ functional_tests/test_api.py
@@ -1,6 +1,24 @@
+from django.contrib.auth import get_user_model
 from django.core.urlresolvers import resolve
+from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase
 from windows.models import Window
+
+# TODO: Api must require authentication to access
+# TODO: Api must require permissions for some actions like POST, PUT, DELETE?
+
+User = get_user_model()
+
+
+class APIAuthentication(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user('user', 'user@mail.com', '123')
+        self.token = Token.objects.get(user=self.user).key
+
+    def test_authorization(self):
+        header = {'HTTP_AUTHORIZATION': 'Token {}'.format(self.token)}
+        response = self.client.get('/api/', {}, **header)
+        self.assertEqual(response.status_code, 200, "REST token-auth failed")
 
 
 class WindowAPITestCase(APITestCase):
@@ -28,7 +46,21 @@ class WindowAPITestCase(APITestCase):
         route = resolve('/api/windows/')
         self.assertEqual(route.func.__name__, 'WindowViewSet')
 
-    def test_create_solo(self):
+    def test_unautheticated_cant_create_window(self):
+        """Test that unauthenticated user can't create a Window"""
+        post_data = {
+            'title': "Window 3",
+            'slug': 'window-3',
+            'description': "The third window",
+        }
+        response = self.client.post('/api/windows/', data=post_data, format='json')
+        self.assertEqual(response.status_code, 403, response.data)
+        unauthenticated_error = {
+            u'detail': u'Authentication credentials were not provided.'
+        }
+        self.assertDictEqual(response.data, unauthenticated_error)
+
+    def test_create_window(self):
         """Test that we can create a Window"""
         post_data = {
             'title': "Window 3",
